20f11a4483abfd844bba2c470d89ab0139e21d39
Rajeev Ranjan <rajeevajranjan@gmail.com>
2025-07-16 23:11:58 +0530

    Variable Demo

---
+++ 002-Variables.py
@@ -0,0 +1,35 @@
+# Multi-line comments
+"""
+Variable Names:
+ - Starts with an alphabet.
+ - Numbers and underscores are allowed.
+ - Cannot have spaces within it.
+"""
+
+"""
+String Rules:
+ - Strings can be in double quotes.
+ - Strings can be in single quote.
+ - Strings can contain another string.
+ - Strings can contain any characters.
+"""
+
+message1 = "I like Python programming very  much!!! -- Rajeev Ranjan"
+__message2 = "Hello World"
+# Print() API Reference: https://docs.python.org/3/library/functions.html#print
+print(message1)
+print(__message2)
+
+student_name = "Namish Ranjan"
+Student_Name = 'Divyansh Maurya'
+print(student_name)
+print(Student_Name)
+
+message3 = "I like 'Python programming' very  much!!! -- Rajeev Ranjan"
+print(message3)
+
+message4 = "I like \"Python programming\" very  much!!! -- Rajeev Ranjan"
+print(message4)
+
+message5 = 'I like \"Python programming\" very  much!!! -- Rajeev Ranjan'
+print(message5)
