69f373882e44f57096765c312dbf2d5e67c34a76
Rajeev Ranjan <rajeevajranjan@gmail.com>
2025-07-16 23:11:31 +0530

    String Demo

---
+++ 003-String-Demo.py
@@ -0,0 +1,21 @@
+# String Documentation https://docs.python.org/3/library/string.html
+# String Methods: https://docs.python.org/3/library/stdtypes.html#string.methods
+
+name = "rajeev ranjan"
+print(name)
+
+print(name.upper())
+print(name.title())
+
+# Formatting String
+name = "Rajeev Ranjan"
+print(f"Hello  {name}")
+
+first_name = "Rajeev"
+last_name = "Ranjan"
+print(f"Hello {first_name} {last_name}") # f is for the format string or f-string.
+
+print("Hello" + " " + first_name +  " " + last_name) # Concatenation
+
+print (10 + 20) # Integer addition
+print ("10" + "20") # Concatenation
